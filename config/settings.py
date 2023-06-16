d22c4f110f13cdb98a55a5f549fff656148705fb
Mashhurbek Mashrabov <72388700+mashhurbek-mashrabov@users.noreply.github.com>
2023-06-16 11:33:33 +0500

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -140,7 +140,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/tatu/static/'
 
 STATIC_ROOT = str(BASE_DIR.joinpath('vol', 'static'))
 STATICFILES_DIRS = [
